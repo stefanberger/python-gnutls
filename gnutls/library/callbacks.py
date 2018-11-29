c76b22be0d25260252ef979e603e4d577b6c02a0
Stefan Berger <stefanb@linux.vnet.ibm.com>
2018-11-29 16:46:03 -0500

    Add functions to register and deregister keyuri pin callbacks
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- gnutls/library/callbacks.py
+++ gnutls/library/callbacks.py
@@ -2,9 +2,16 @@
 from gnutls.library.constants import GNUTLS_PIN_WRONG
 
 _keyuri_pins = []
+_keyuri_cbs = []
 
 
 def gnutls_pin_function_cb(attempts, token_url, token_label, flags):
+
+    # Process application callbacks
+    for cb, data in _keyuri_cbs:
+        pin, _ = cb(attempts, token_url, token_label, flags, data)
+        if pin:
+            return pin, 0
 
     if (flags & GNUTLS_PIN_WRONG) != 0:
         return None, -1
