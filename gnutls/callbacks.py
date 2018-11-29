1e8ef373310fd68e9c3aae5f3b827e748a9dada1
Stefan Berger <stefanb@linux.vnet.ibm.com>
2018-11-29 16:46:03 -0500

    Add functions to register and deregister keyuri pin callbacks
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- gnutls/callbacks.py
+++ gnutls/callbacks.py
@@ -1,5 +1,5 @@
 
-from gnutls.library.callbacks import _keyuri_pins
+from gnutls.library.callbacks import _keyuri_pins, _keyuri_cbs
 
 def gnutls_set_pin_for_keyuri(keyuri, pin):
     global _keyuri_pins
@@ -8,3 +8,11 @@ def gnutls_set_pin_for_keyuri(keyuri, pin):
 def gnutls_remove_pin_for_keyuri(keyuri):
     global _keyuri_pins
     _keyuri_pins = [x for x in _keyuri_pins if not (x[0] == keyuri)]
+
+def gnutls_register_pin_for_keyuri_callback(cb, data):
+    global _keyuri_cbs
+    _keyuri_cbs.append((cb, data))
+
+def gnutls_deregister_pin_for_keyuri_callback(cb):
+    global _keyuri_cbs
+    _keyuri_cbs = [x for x in _keyuri_cbs if not (x[0] == cb)]
--- gnutls/library/callbacks.py
+++ gnutls/library/callbacks.py
@@ -3,9 +3,17 @@ import os
 from gnutls.library.constants import GNUTLS_PIN_USER, GNUTLS_PIN_WRONG
 
 _keyuri_pins = []
+_keyuri_cbs = []
 
 def gnutls_pin_function_cb(attempts, token_url, token_label, flags):
     global _keyuri_pins
+    global _keyuri_cbs
+
+    # Process application callbacks
+    for cb, data in _keyuri_cbs:
+        pin, ret = cb(attempt, token_url, token_label, flags, data)
+        if pin:
+            return pin, 0
 
     if (flags & GNUTLS_PIN_WRONG) != 0:
         return None, -1
