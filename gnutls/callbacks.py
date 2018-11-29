e81424d2e477873074c6090a90e3b309222a9d58
Stefan Berger <stefanb@linux.vnet.ibm.com>
2018-11-29 16:46:03 -0500

    Add functions to register and deregister keyuri pin callbacks
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- gnutls/callbacks.py
+++ gnutls/callbacks.py
@@ -1,5 +1,4 @@
 
-
 def gnutls_set_pin_for_keyuri(keyuri, pin):
     from gnutls.library.callbacks import _keyuri_pins
     _keyuri_pins.append((keyuri, pin))
@@ -8,3 +7,13 @@ def gnutls_set_pin_for_keyuri(keyuri, pin):
 def gnutls_remove_pin_for_keyuri(keyuri):
     from gnutls.library.callbacks import _keyuri_pins
     _keyuri_pins = [x for x in _keyuri_pins if not (x[0] == keyuri)]
+
+
+def gnutls_register_pin_for_keyuri_callback(cb, data):
+    from gnutls.library.callbacks import _keyuri_cbs
+    _keyuri_cbs.append((cb, data))
+
+
+def gnutls_deregister_pin_for_keyuri_callback(cb):
+    from gnutls.library.callbacks import _keyuri_cbs
+    _keyuri_cbs = [x for x in _keyuri_cbs if not (x[0] == cb)]
